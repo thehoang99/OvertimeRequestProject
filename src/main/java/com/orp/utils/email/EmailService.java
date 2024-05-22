8ea5ab4611077135d58d085d7d2a9f83942a0957
The Hoang <thehoangyaumiao@gmail.com>
2024-05-22 09:17:08 +0700

    add feature send email ok

--- src/main/java/com/orp/utils/email/EmailService.java
+++ src/main/java/com/orp/utils/email/EmailService.java
@@ -5,10 +5,8 @@ import com.orp.dto.ClaimEmailDTTO;
 import java.util.List;
 
 public interface EmailService {
-
     String  EMAIL_TEMP = "/layout/email/emailTemplate";
 
     void sendHtmlEmail(ClaimEmailDTTO claimEmailDTTO, List<String> toList, String receiver, String mailTemplate, String url, String content);
 
-    void sendEmailPendingClaimToApprover(List<ClaimEmailDTTO> claimEmailDTOS, String to, String receiver, String mailTemplate, String subject);
 }
